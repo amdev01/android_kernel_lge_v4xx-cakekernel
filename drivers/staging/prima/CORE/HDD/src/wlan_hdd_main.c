c5ac7f050959d471ffbdb460cdc2de506e98bf95
Adam Myczkowski (mycax) <mycaxd511@gmail.com>
2017-10-25 14:20:22 -0400

    Fix prima missmerge (?)
    Signed-off-by: Adam Myczkowski (mycax) <mycaxd511@gmail.com>

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
@@ -9015,8 +9015,10 @@ err_bap_close:
 
 err_close_adapter:
    hdd_close_all_adapters( pHddCtx );
+#ifdef CONFIG_ENABLE_LINUX_REG
 err_unregister_wiphy:
    wiphy_unregister(wiphy) ;
+#endif
 err_vosstop:
    vos_stop(pVosContext);
 
